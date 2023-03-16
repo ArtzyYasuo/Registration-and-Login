aedfca66806a8f39808f368b14dbd57f2033fbbf
albirta yassine <albirtayassine70@outlook.fr>
2023-03-16 22:05:23 +0100

    add to resources

---
+++ src/main/java/com/example/registration_and_login/RegistrationAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.registration_and_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginApplication {
+//https://github.com/ArtzyYasuo/Registration-and-Login.git
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationAndLoginApplication.class, args);
+    }
+
+}
